942a23a9d7d2f7a1b96f5554cf569137aceffeb5
660710759 <phonpuak_n@silpakorn.edu>
2025-09-21 17:07:15 +0000

    bookstore101

---
+++ bookstore-app/src/components/NotFound.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import {Link, useNavigate} from "react-router-dom";
+
+const NotFound = () => {
+    const navigate = useNavigate();
+
+    const nandleGoHome = () => {
+        navigate("/");
+    };
+
+    return(
+        <div>
+            <h1>404 - Not Found</h1>
+            <p>The page you are looking for does not exist.</p>
+        </div>
+    );
+
+
+};
+export default NotFound;
